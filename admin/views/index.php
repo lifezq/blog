5c919afe126a06258e51e72df11c0caa41f7595a
Ryan <lifezqy@126.com>
2016-09-14 16:35:03 +0800

    disabled  info show

--- admin/views/index.php
+++ admin/views/index.php
@@ -1,4 +1,4 @@
-<?php if(!defined('EMLOG_ROOT')) {exit('error!');}?>
+<?php if (!defined('EMLOG_ROOT')) {exit('error!');}?>
 <div id="admindex">
 <div id="admindex_main">
     <div id="tw">
@@ -14,12 +14,12 @@
     </div>
 </div>
 <div class="clear"></div>
-<?php if (ROLE == 'admin'):?>
+<?php if (ROLE == 'admin'): ?>
 <div style="margin-top: 20px;">
-<div id="admindex_servinfo">
+<div id="admindex_servinfo" style="width:99%;">
 <h3>站点信息</h3>
 <ul>
-	<li>有<b><?php echo $sta_cache['lognum'];?></b>篇文章，<b><?php echo $sta_cache['comnum_all'];?></b>条评论，<b><?php echo $sta_cache['twnum'];?></b>条微语</li>
+	<li>有<b><?php echo $sta_cache['lognum']; ?></b>篇文章，<b><?php echo $sta_cache['comnum_all']; ?></b>条评论，<b><?php echo $sta_cache['twnum']; ?></b>条微语</li>
 	<li>PHP版本：<?php echo $php_ver; ?></li>
 	<li>MySQL版本：<?php echo $mysql_ver; ?></li>
 	<li>服务器环境：<?php echo $serverapp; ?></li>
@@ -27,30 +27,30 @@
 	<li>服务器允许上传最大文件：<?php echo $uploadfile_maxsize; ?></li>
 	<li><a href="index.php?action=phpinfo">更多信息&raquo;</a></li>
 </ul>
-<p id="m"><a title="用手机访问你的站点"><?php echo BLOG_URL.'m'; ?></a></p>
+<p id="m"><a title="用手机访问你的站点"><?php echo BLOG_URL . 'm'; ?></a></p>
 </div>
-<div id="admindex_msg">
+<!-- <div id="admindex_msg">
 <h3>官方消息</h3>
 <ul></ul>
-</div>
+</div> -->
 <div class="clear"></div>
 </div>
 </div>
 <script>
-$(document).ready(function(){
-	$("#admindex_msg ul").html("<span class=\"ajax_remind_1\">正在读取...</span>");
-	$.getJSON("http://www.emlog.net/services/messenger.php?v=<?php echo Option::EMLOG_VERSION; ?>&callback=?",
-	function(data){
-		$("#admindex_msg ul").html("");
-		$.each(data.items, function(i,item){
-			var image = '';
-			if (item.image != ''){
-				image = "<a href=\""+item.url+"\" target=\"_blank\" title=\""+item.title+"\"><img src=\""+item.image+"\"></a><br />";
-			}
-			$("#admindex_msg ul").append("<li class=\"msg_type_"+item.type+"\">"+image+"<span>"+item.date+"</span><a href=\""+item.url+"\" target=\"_blank\">"+item.title+"</a></li>");
-		});
-	});
-});
+// $(document).ready(function(){
+// 	$("#admindex_msg ul").html("<span class=\"ajax_remind_1\">正在读取...</span>");
+// 	$.getJSON("http://www.emlog.net/services/messenger.php?v=<?php echo Option::EMLOG_VERSION; ?>&callback=?",
+// 	function(data){
+// 		$("#admindex_msg ul").html("");
+// 		$.each(data.items, function(i,item){
+// 			var image = '';
+// 			if (item.image != ''){
+// 				image = "<a href=\""+item.url+"\" target=\"_blank\" title=\""+item.title+"\"><img src=\""+item.image+"\"></a><br />";
+// 			}
+// 			$("#admindex_msg ul").append("<li class=\"msg_type_"+item.type+"\">"+image+"<span>"+item.date+"</span><a href=\""+item.url+"\" target=\"_blank\">"+item.title+"</a></li>");
+// 		});
+// 	});
+// });
 </script>
 <?php endif;?>
 <script>
